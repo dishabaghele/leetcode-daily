8e64d3358a4e226defd09f78aed41d8c982d7b09
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2025-07-10 13:16:38 +0530

    Time: 3 ms (70.56%) | Memory: 126 MB (19.47%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -4,17 +4,17 @@ public:
         vector<int>freeTime;
         freeTime.push_back(startTime[0]);
         for(int i=1; i<startTime.size(); i++){
-            freeTime.push_back(startTime[i] - endTime[i-1]);
+            freeTime.push_back(startTime[i]-endTime[i-1]);
         }
-        freeTime.push_back(eventTime - endTime[endTime.size()-1]);
+        freeTime.push_back(eventTime-endTime[endTime.size()-1]);
         int currSum = 0;
         int maxSum = 0;
-        int i=0;
-        int j=0;
+        int i = 0;
+        int j = 0;
         int n = freeTime.size();
-        while(j < n){
+        while(j<n){
             currSum += freeTime[j];
-            if(i<n && j-i+1 > k+1){
+            if(i < n && j-i+1 > k+1){
                 currSum -= freeTime[i];
                 i++;
             }
