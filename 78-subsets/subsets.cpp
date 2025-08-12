c3113cb43e2ec3e4f72462498ef5f885a2dae513
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2025-08-13 01:30:23 +0530

    Time: 259 ms (5.60%) | Memory: 108.1 MB (6.35%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    void genSubset(int ind, vector<int>& nums, vector<int>& subset, vector<vector<int>>& ans){
+    vector<vector<int>>genSubset(int ind, vector<int>&nums, vector<int>&subset, vector<vector<int>>&subsetArray){
         if(ind == nums.size()){
-            ans.push_back(subset);
-            return;
+            subsetArray.push_back(subset);
+            return subsetArray;
         }
         //take
         subset.push_back(nums[ind]);
-        genSubset(ind+1, nums, subset, ans);
+        genSubset(ind+1, nums, subset, subsetArray);
         subset.pop_back();
+
         //not take
-        genSubset(ind+1, nums, subset, ans);
+        genSubset(ind+1, nums, subset, subsetArray);
+        return subsetArray;
     }
 
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>ans;
+        vector<vector<int>>subsetArray;
         vector<int>subset;
-        genSubset(0, nums, subset, ans);
-        return ans;
+        return genSubset(0, nums, subset, subsetArray);
     }
 };
