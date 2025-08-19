3b12af229ab44180f91e42e44965e2c21557a166
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2025-08-19 23:49:06 +0530

    Time: 3 ms (40.46%) | Memory: 14.4 MB (24.75%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    void findCombinationSum(int ind, vector<int>& candidates, int target, vector<int>& subset, set<vector<int>>&ans){
+    void genAllSubset(int ind, vector<int>& candidates, int target, vector<int>&subset, set<vector<int>>& ans){
         if(target == 0){
             ans.insert(subset);
             return;
         }
+           
         if(ind == candidates.size() || candidates[ind] > target){
             return;
         }
-        
-        if(target >= candidates[ind]){
+       
+        //pick
+        if(candidates[ind] <= target){
             subset.push_back(candidates[ind]);
-            findCombinationSum(ind+1, candidates, target-candidates[ind], subset, ans);
+            genAllSubset(ind+1, candidates, target-candidates[ind], subset, ans);
             subset.pop_back();
         }
-        int next = ind+1;
-        while(next < candidates.size() && candidates[next] == candidates[ind]){
-            next++;
-        }
-        findCombinationSum(next, candidates, target, subset, ans);
 
+        int next = ind + 1;
+
+        while(next < candidates.size() && candidates[ind] == candidates[next]) next++;
+       
+        //not pick
+        genAllSubset(next, candidates, target, subset, ans);
     }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
         set<vector<int>>ans;
         vector<int>subset;
-        findCombinationSum(0, candidates, target, subset, ans);
-        vector<vector<int>>final(ans.begin(), ans.end());
-        return final;
+        sort(candidates.begin(), candidates.end());
+        genAllSubset(0, candidates, target, subset, ans);
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
