5863a06b4f659df8c32107450071e294b8f4d6ce
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2025-08-20 18:58:32 +0530

    Time: 1 ms (52.81%) | Memory: 13.9 MB (83.13%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    void genAllSubset(int ind, vector<int>& candidates, int target, vector<int>&subset, set<vector<int>>& ans){
+    void findAllCombinationSumSubset(int ind, vector<int>& candidates, int target, vector<int>& subset, vector<vector<int>>& ans){
         if(target == 0){
-            ans.insert(subset);
+            ans.push_back(subset);
             return;
         }
-           
-        if(ind == candidates.size() || candidates[ind] > target){
-            return;
-        }
-       
-        //pick
-        if(candidates[ind] <= target){
-            subset.push_back(candidates[ind]);
-            genAllSubset(ind+1, candidates, target-candidates[ind], subset, ans);
+        if(ind == candidates.size())
+        return;
+        for(int i=ind; i<candidates.size(); i++){
+            if(i > ind && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i] > target) break;
+            subset.push_back(candidates[i]);
+            findAllCombinationSumSubset(i+1, candidates, target-candidates[i], subset, ans);
             subset.pop_back();
         }
 
-        int next = ind + 1;
-
-        while(next < candidates.size() && candidates[ind] == candidates[next]) next++;
-       
-        //not pick
-        genAllSubset(next, candidates, target, subset, ans);
     }
-
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        set<vector<int>>ans;
+        vector<vector<int>>ans;
         vector<int>subset;
         sort(candidates.begin(), candidates.end());
-        genAllSubset(0, candidates, target, subset, ans);
-        return vector<vector<int>>(ans.begin(), ans.end());
+        findAllCombinationSumSubset(0, candidates, target, subset, ans);
+        return ans;
     }
 };
