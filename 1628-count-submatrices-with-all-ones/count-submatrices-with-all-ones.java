bff133b6a8337d52fe35820c6476bcf884d20caf
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2025-08-22 13:51:38 +0530

    Time: 7 ms (65.65%) | Memory: 45 MB (94.90%) - LeetSync

---
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numSubmat(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int[][] newmat=new int[n][m];
+        for(int i=0;i<n;i++){
+            newmat[i][m-1]= mat[i][m-1]==0 ? 0 : 1;
+            for(int j=m-2;j>=0;j--){
+                newmat[i][j]= mat[i][j]==0 ? 0 : newmat[i][j+1]+1;
+            }
+        }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         System.out.print(newmat[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int minwidth=newmat[i][j];
+                for(int d=i;d<n;d++){
+                    // System.out.println("for newmat "+ newmat[i][d]+"minwidth is : "+minwidth);
+                    if(newmat[d][j]==0) {
+                        break;
+                    }
+                    minwidth=Math.min(newmat[d][j],minwidth);
+                    
+                    count+=minwidth;
+                }
+            }
+        }
+        return count;
+    }
+}
