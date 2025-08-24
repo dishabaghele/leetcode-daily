065366ea4b13f5de310083ae65894e8de4c1afa2
dishabaghele <94739282+dishabaghele@users.noreply.github.com>
2025-08-25 01:08:47 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (54.55%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    void genAllSubset(int ind, vector<int>& candidates, int target, vector<int>&subset, vector<vector<int>>& ans){
-        if(ind == candidates.size()){
-            if(target == 0){
-                ans.push_back(subset);
-            }
+    void findCombSum(int ind, vector<int>& candidates, int target, vector<int>& subset, vector<vector<int>>& ans){
+        if(target == 0){
+            ans.push_back(subset);
             return;
         }
+        if(ind == candidates.size()) return;
+        
         //pick
-        if(candidates[ind] <= target){
+        if(target >= candidates[ind]){
             subset.push_back(candidates[ind]);
-            genAllSubset(ind, candidates, target-candidates[ind], subset, ans);
+            findCombSum(ind, candidates, target-candidates[ind], subset, ans);
             subset.pop_back();
         }
-       
-        //not pick
-        genAllSubset(ind+1, candidates, target, subset, ans);
+        
+
+        findCombSum(ind+1, candidates, target, subset, ans);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
         vector<int>subset;
-        genAllSubset(0, candidates, target, subset, ans);
+        findCombSum(0, candidates, target, subset, ans);
         return ans;
     }
 };
