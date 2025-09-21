45ea90ebbe45f0eb56c18d8f2256362751aeb703
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2025-09-22 00:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (60.12%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    void findAllCombination(int ind, vector<int>& candidates, int target, vector<int>& subset, vector<vector<int>>& ans){
+    void findAllCombinationSum(int ind, vector<int>& candidates, int target, vector<int>& subset, vector<vector<int>>&ans){
         if(target == 0){
             ans.push_back(subset);
             return;
         }
+        if(ind == candidates.size()){
+            return;
+        }
         for(int i=ind; i<candidates.size(); i++){
             if(i > ind && candidates[i-1] == candidates[i])continue;
-            if(target < candidates[i]) break;
+            if(candidates[i] > target) break;
             subset.push_back(candidates[i]);
-            findAllCombination(i+1, candidates, target-candidates[i], subset, ans);
+            findAllCombinationSum(i+1, candidates, target-candidates[i], subset, ans);
             subset.pop_back();
         }
     }
@@ -18,7 +21,7 @@ public:
         vector<vector<int>>ans;
         vector<int>subset;
         sort(candidates.begin(), candidates.end());
-        findAllCombination(0, candidates, target, subset, ans);
-        return ans; 
+        findAllCombinationSum(0, candidates, target, subset, ans);
+        return ans;
     }
 };
