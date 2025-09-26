feaaf2039e84b886673111687301d3d92143380b
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2025-09-26 15:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 15.5 MB (69.25%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void findAllParenthesis(int n, string s, int open, int close, vector<string>& ans){
-        if(s.size() == n*2){
+    void genAllParenthesis(int n, string s, int open, int close, vector<string>& ans){
+        if(s.size() == 2*n){
             ans.push_back(s);
             return;
         }
-        //open bracket
-        if(open < n){
-            s.push_back('(');
-            findAllParenthesis(n, s, open+1, close, ans);
-            s.pop_back();
-        }
-        //close bracket
-        if(close < open){
-            s.push_back(')');
-            findAllParenthesis(n, s, open, close+1, ans);
-            s.pop_back();
-        }
-    }
 
+        if(close > open) return;
+       
+        if(open < n){
+            genAllParenthesis(n, s + '(', open + 1, close, ans);
+            
+        }
+        genAllParenthesis(n, s + ')', open, close + 1, ans);
+        
+        
+    }
     vector<string> generateParenthesis(int n) {
         vector<string>ans;
-        findAllParenthesis(n, "", 0, 0, ans);
+        genAllParenthesis(n, "", 0, 0, ans);
         return ans;
     }
 };
