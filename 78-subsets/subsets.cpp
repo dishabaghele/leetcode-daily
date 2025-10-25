1ee78d2619a2124afb1bf0a8ffdc448969f05c2d
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2025-10-25 21:09:14 +0530

    Time: 3 ms (30.72%) | Memory: 13.7 MB (22.72%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void findAllSubset(int ind, vector<int>& nums, vector<int>& subset, vector<vector<int>>& ans){
-        if(ind == nums.size()){
-            ans.push_back(subset);
-            return;
-        }
-        //pick
+    void findAllSubset(int ind, vector<int>& nums, vector<int>& subset, set<vector<int>>&ans){
+        ans.insert(subset);
+        if(ind == nums.size()) return;
+        //pick condition
         subset.push_back(nums[ind]);
         findAllSubset(ind+1, nums, subset, ans);
         subset.pop_back();
+
+        //not pick condition
         findAllSubset(ind+1, nums, subset, ans);
     }
 
-    vector<vector<int>> subsets(vector<int>& nums) { 
-        vector<vector<int>>ans;
+    vector<vector<int>> subsets(vector<int>& nums) {
+        set<vector<int>>ans;
         vector<int>subset;
         findAllSubset(0, nums, subset, ans);
-        return ans;  
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
