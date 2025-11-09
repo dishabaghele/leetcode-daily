82c6e3efd5562b8b3711e8dbe653aa0f77f9f101
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2025-11-10 00:16:53 +0530

    Time: 3 ms (54.19%) | Memory: 14.1 MB (53.28%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -7,19 +7,18 @@ public:
         }
         if(ind == candidates.size()) return;
 
-        //pick
-        if(candidates[ind] <= target){
+        //take
+        if(target >= candidates[ind]){
             subset.push_back(candidates[ind]);
-            findAllCombination(ind, candidates, target - candidates[ind], subset, ans);
+            findAllCombination(ind, candidates, target-candidates[ind], subset, ans);
             subset.pop_back();
         }
-
-        //not pick
+        
+        //not take
         findAllCombination(ind+1, candidates, target, subset, ans);
     }
-    
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
+        vector<vector<int>>ans;
         vector<int>subset;
         findAllCombination(0, candidates, target, subset, ans);
         return ans;
