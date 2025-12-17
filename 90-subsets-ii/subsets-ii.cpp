c67b3a0c71ee2fe2fffb30b97722d4cd6f8f9fb5
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2025-12-18 00:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (27.03%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void findAllCombination(int ind, vector<int>&nums, vector<int>&subset, set<vector<int>>&ans){
+    void findAllSub(int ind, vector<int>& nums, vector<int>& subset, set<vector<int>>&ans){
         ans.insert(subset);
         if(ind == nums.size()) return;
         for(int i=ind; i<nums.size(); i++){
-            if(i > ind && nums[i-1] == nums[i]) continue;
+            if(ind != i && nums[i-1] == nums[i])continue;
             subset.push_back(nums[i]);
-            findAllCombination(i+1, nums, subset, ans);
+            findAllSub(i+1, nums, subset, ans);
             subset.pop_back();
         }
     }
-    
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         set<vector<int>>ans;
+        sort(nums.begin(), nums.end());
         vector<int>subset;
-        findAllCombination(0, nums, subset, ans);
+        findAllSub(0, nums, subset, ans);
         return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
