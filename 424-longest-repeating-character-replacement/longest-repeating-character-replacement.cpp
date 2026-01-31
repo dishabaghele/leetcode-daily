743898aca9bd0e60c784fe354b06269287a7fae7
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2026-02-01 01:48:46 +0530

    Time: 6 ms (48.17%) | Memory: 10.8 MB (92.21%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+        int maxFreq = 0;
+        vector<int>hashSet(26, 0);
+        while(r < s.size()){
+            hashSet[s[r]-'A']++;
+            maxFreq = max(maxFreq, hashSet[s[r]-'A']);
+            while((r-l+1) - maxFreq > k){
+                hashSet[s[l]-'A']--;
+                l++;
+            }
+            maxLen = max(maxLen, r-l+1);
+            r++;
+        }
+        return maxLen;
+    }
+};
