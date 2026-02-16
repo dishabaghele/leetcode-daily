fde8f86256792b8776d9dff2a5229c5562305bc8
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2026-02-16 18:48:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (87.76%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    void findAllPermutation(vector<int>& nums, vector<int>& subset, vector<int>& freq, vector<vector<int>>&ans){
-        if(subset.size() == nums.size()){
-            ans.push_back(subset);
+    void findAllPerm(int ind, vector<int>& nums, vector<vector<int>>&ans){
+        if(ind == nums.size()){
+            ans.push_back(nums);
             return;
         }
-
-        for(int i=0; i<nums.size(); i++){
-            if(freq[i] == 1) continue;
-            subset.push_back(nums[i]);
-            freq[i] = 1;
-            findAllPermutation(nums, subset, freq, ans);
-            subset.pop_back();
-            freq[i] = 0;
+        for(int i=ind; i<nums.size(); i++){
+            swap(nums[i], nums[ind]);
+            findAllPerm(ind+1, nums, ans);
+            swap(nums[i], nums[ind]);
         }
     }
-
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
-        vector<int>freq(nums.size(), 0);
-        vector<int>subset;
-        findAllPermutation(nums, subset, freq, ans);
+        findAllPerm(0, nums, ans);
         return ans;
     }
 };
