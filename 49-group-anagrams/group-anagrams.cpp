8a8412ee9c7a4da03e0c714468757910e08e9c5c
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2026-03-17 19:23:42 +0530

    Time: 13 ms (77.52%) | Memory: 25.9 MB (54.73%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>>ans;
         unordered_map<string, vector<string>>freq;
-        vector<string>dup = strs;
         for(int i=0; i<strs.size(); i++){
-            sort(dup[i].begin(), dup[i].end());
-            freq[dup[i]].push_back(strs[i]);
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            freq[s].push_back(strs[i]);
         }
         for(auto i : freq){
             ans.push_back(i.second);
