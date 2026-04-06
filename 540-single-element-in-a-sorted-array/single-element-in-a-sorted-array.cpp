51c8e129b839c77e0c5678b2c3439d1d6ea8e87e
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2026-04-07 01:08:26 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (34.43%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,24 +4,27 @@ public:
         int n = nums.size();
         if(n == 1) return nums[0];
         if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
-        int l = 1;
-        int r = nums.size()-2;
-        while(l <= r){
-            int mid = (l+r)/2;
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]) return nums[mid];
-            if(mid%2 == 1){
-                if(nums[mid] == nums[mid-1])
-                l = mid + 1;
-                else
-                r = mid - 1;
+        if(nums[n-1] != nums[n-2]) return nums[n-1]; 
+        int left = 1, right = n-2;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                return nums[mid];
+            }else if(mid % 2 != 0){
+                if(nums[mid] == nums[mid-1]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
             }else{
-                if(nums[mid] == nums[mid-1])
-                r = mid - 1;
-                else
-                l = mid + 1;
+                if(nums[mid] == nums[mid-1]){
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+
             }
         }
-        return -1;  
+        return -1;
     }
 };
