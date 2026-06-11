1725bb557a2de10231fba00f05aa7e8e40fccf28
Disha Baghele <94739282+dishabaghele@users.noreply.github.com>
2026-06-11 23:12:27 +0530

    Time: 1 ms (24.08%) | Memory: 53 MB (20.15%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,13 +1,9 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        HashMap<Integer, Integer>freq = new HashMap<>();
+        int xor = 0;
         for(int i=0; i<nums.length; i++){
-            freq.put(nums[i], freq.getOrDefault(nums[i], 0)+1);
+            xor ^= nums[i];
         }
-        for(Map.Entry<Integer, Integer> it : freq.entrySet()){
-            if(it.getValue() == 1)
-            return it.getKey();
-        }
-        return -1; 
+        return xor;
     }
 }
